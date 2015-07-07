218113b21da4d2fb3bc48617df2dcf6063fd14b0
Kylie Stradley <kylie.stradley@gmail.com>
2015-07-07 00:16:37 -0400

    Move cell class to separate file

--- game.rb
+++ game.rb
@@ -1,40 +1,5 @@
 require 'pry'
-
-class Cell
-  def initialize(x, y, alive)
-    @x = x
-    @y = y
-    @alive = alive
-  end
-
-  def x
-    @x
-  end
-
-  def y
-    @y
-  end
-
-  def alive
-    @alive
-  end
-
-  def neighbors
-   [ Cell.new(self.x, self.y-1, false), Cell.new(self.x, self.y+1, false),
-     Cell.new(self.x-1, self.y-1, false), Cell.new(self.x-1, self.y, false), Cell.new(self.x-1, self.y+1, false),
-     Cell.new(x+1, y-1, false), Cell.new(self.x+1, self.y, false), Cell.new(self.x+1, self.y+1, false)
-   ]
-  end
-
-  def to_s
-    if alive
-      'x'
-    else
-      ' '
-    end
-  end
-end
-
+require './cell.rb'
 class Game
   def initialize(seed, generations)
     @generations = generations
